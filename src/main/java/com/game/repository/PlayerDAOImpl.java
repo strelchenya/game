50c8a5d1979a35108fc1cd6491f68d4f842b35cd
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-08-25 17:43:41 +0300

    Update PlayerDAOImpl

--- src/main/java/com/game/repository/PlayerDAOImpl.java
+++ src/main/java/com/game/repository/PlayerDAOImpl.java
@@ -4,19 +4,20 @@ import com.game.entity.Player;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class PlayerDAOImpl implements PlayerDAO{
+public class PlayerDAOImpl implements PlayerDAO {
 
+    @Autowired
     private SessionFactory sessionFactory;
 
     @Override
     public List<Player> getAllEmployees() {
         Session session = sessionFactory.getCurrentSession();
-
         Query<Player> query = session.createQuery("from Player", Player.class);
         List<Player> allPlayers = query.getResultList();
         return allPlayers;
@@ -30,14 +31,17 @@ public class PlayerDAOImpl implements PlayerDAO{
 
     @Override
     public Player getEmployee(Long id) {
-
         Session session = sessionFactory.getCurrentSession();
         Player player = session.get(Player.class, id);
-        return null;
+        return player;
     }
 
     @Override
     public void deleteEmployee(Long id) {
-
+        Session session = sessionFactory.getCurrentSession();
+        Query<Player> query = session.createQuery("DELETE FROM Player " +
+                                                            "WHERE id =:playerId");
+        query.setParameter("playerId", id);
+        query.executeUpdate();
     }
 }
