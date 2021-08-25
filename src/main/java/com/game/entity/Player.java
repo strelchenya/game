bafebd358e15b0d70c0f4a6a8f93cccba98dd0e4
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-08-25 14:44:23 +0300

    Create entity Player and PlayerRepository

---
+++ src/main/java/com/game/entity/Player.java
@@ -0,0 +1,142 @@
+package com.game.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "players")
+public class Player extends BaseEntity{
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "race")
+    private Race race;
+    @Column(name = "profession")
+    private Profession profession;
+    @Column(name = "experience")
+    private Integer experience;
+    @Column(name = "level")
+    private Integer level;
+    @Column(name = "untilNextLevel")
+    private Integer untilNextLevel;
+    @Column(name = "birthday")
+    private Date birthday;
+    @Column(name = "banned")
+    private Boolean banned;
+
+    public Player() {
+    }
+
+    public Player(
+            String name,
+            String title,
+            Race race,
+            Profession profession,
+            Integer experience,
+            Integer level,
+            Integer untilNextLevel,
+            Date birthday,
+            Boolean banned) {
+        this.name = name;
+        this.title = title;
+        this.race = race;
+        this.profession = profession;
+        this.experience = experience;
+        this.level = level;
+        this.untilNextLevel = untilNextLevel;
+        this.birthday = birthday;
+        this.banned = banned;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Race getRace() {
+        return race;
+    }
+
+    public void setRace(Race race) {
+        this.race = race;
+    }
+
+    public Profession getProfession() {
+        return profession;
+    }
+
+    public void setProfession(Profession profession) {
+        this.profession = profession;
+    }
+
+    public Integer getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Integer experience) {
+        this.experience = experience;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Integer getUntilNextLevel() {
+        return untilNextLevel;
+    }
+
+    public void setUntilNextLevel(Integer untilNextLevel) {
+        this.untilNextLevel = untilNextLevel;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Boolean getBanned() {
+        return banned;
+    }
+
+    public void setBanned(Boolean banned) {
+        this.banned = banned;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", title='" + title + '\'' +
+                ", race=" + race +
+                ", profession=" + profession +
+                ", experience=" + experience +
+                ", level=" + level +
+                ", untilNextLevel=" + untilNextLevel +
+                ", birthday=" + birthday +
+                ", banned=" + banned +
+                '}';
+    }
+}
